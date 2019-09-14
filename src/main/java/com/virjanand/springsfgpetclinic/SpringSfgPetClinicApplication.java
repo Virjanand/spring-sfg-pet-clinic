5338c9af5d798c7c1e1d055ffa0312e4874ecc7c
Virjanand <virjanand@virjanand.com>
2019-09-14 14:20:42 +0200

    Setup Initial Project

---
+++ src/main/java/com/virjanand/springsfgpetclinic/SpringSfgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.virjanand.springsfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSfgPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSfgPetClinicApplication.class, args);
+    }
+
+}
