45011ec21249acaab7bbf3009181a07e9a54dbfc
Virjanand <virjanand@virjanand.com>
2019-09-23 11:41:46 +0200

    added pet type service. Closes #29

---
+++ pet-clinic-data/src/main/java/com/virjanand/springsfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.virjanand.springsfgpetclinic.services;
+
+import com.virjanand.springsfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
