8f596365bdec7497dd3ce30fb1d48d6bf0336e32
Virjanand <virjanand@virjanand.com>
2019-09-23 12:14:13 +0200

    Added specialities map impl. Closes #31

---
+++ pet-clinic-data/src/main/java/com/virjanand/springsfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package com.virjanand.springsfgpetclinic.services;
+
+import com.virjanand.springsfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends  CrudService<Speciality, Long> {
+}
